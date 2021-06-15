dd2700b383dfea131b26db210d0c50db3248d643
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 11:28:33 +0200

    change to build both ESM and UMD modules

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,19 @@ import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './svelte-viewport-info.ts',
-  output: {
-    dir: './',
-    format: 'umd',  // builds for both Node.js and Browser
-    name:'Viewport', // required for UMD modules
-    noConflict:true,
-    sourcemap: true,
-    exports: 'default',
-  },
+  output: [
+    {
+      dir:       './',
+      format:    'umd', // builds for both Node.js and Browser
+      name:      'Viewport', // required for UMD modules
+      noConflict:true,
+      sourcemap: true,
+      exports:   'default',
+    },{
+      file:     './svelte-viewport-info.esm.js',
+      format:   'esm',
+      sourcemap:true
+    }
+  ],
   plugins: [typescript(), terser()],
 };
